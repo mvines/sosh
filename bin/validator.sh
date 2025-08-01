3dc7f7cf26b3eb44c412c6967196f86ec228348a
Michael Vines <m@vin.es>
2025-08-01 10:55:54 -0700

    Remove --accounts-db-skip-shrink

--- bin/validator.sh
+++ bin/validator.sh
@@ -56,7 +56,6 @@ args=(
   --skip-poh-verify
   --no-poh-speed-test
   --wal-recovery-mode skip_any_corrupted_record
-  --accounts-db-skip-shrink
   --full-snapshot-interval-slots 12000
   --maximum-full-snapshots-to-retain 1
   --maximum-incremental-snapshots-to-retain 1
